cbbc8a0f92a179d32f781fc5737692b053b5447c
stephen grider <ste.grider@gmail.com>
2016-09-10 15:09:42 -0700

    added header styling

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -4,7 +4,15 @@ import { Text } from 'react-native';
 
 // Make a component
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 // Make the component available to other parts of the app
