a3c273336d8f386da15ac9e96899f1ff840cddb7
stephen grider <ste.grider@gmail.com>
2016-09-10 15:15:36 -0700

    added styling

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,15 +1,22 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
