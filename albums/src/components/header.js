738d27a233e6eb33ea37146430144448d8150848
stephen grider <ste.grider@gmail.com>
2016-09-10 15:27:26 -0700

    more styling

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -15,7 +15,16 @@ const Header = () => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
