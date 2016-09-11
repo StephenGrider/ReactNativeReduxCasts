15215c7b4bb7480f04ffc3dc7c1d47595d8d34a4
stephen grider <ste.grider@gmail.com>
2016-09-11 14:45:29 -0700

    added axios request

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
   }
 
   render() {
