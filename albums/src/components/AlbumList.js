fa38374c596b758a479db412bae2ea6bee9a4edf
stephen grider <ste.grider@gmail.com>
2016-09-11 14:55:46 -0700

    added albums state

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,12 +3,16 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Album List!!!!</Text>
