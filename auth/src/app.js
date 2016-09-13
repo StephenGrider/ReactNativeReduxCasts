2e5645d6e9f4744b9a1e050c9aae708a23ccc536
stephen grider <ste.grider@gmail.com>
2016-09-13 16:06:38 -0700

    both index files use app

---
+++ auth/src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>An App!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
