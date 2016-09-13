752d4bf6a8de71c03416350bc992768c777a20ba
stephen grider <ste.grider@gmail.com>
2016-09-12 20:55:27 -0700

    added touchable opacity

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -27,11 +28,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+      
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
---
+++ albums/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () => {
+  return (
+    <TouchableOpacity>
+      <Text>Click me!!!</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
