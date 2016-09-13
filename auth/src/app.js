84071899d6b182154b613a7c0ac890ff18e3f723
stephen grider <ste.grider@gmail.com>
2016-09-13 16:38:46 -0700

    added firebase init

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
+      authDomain: 'authentication-70a18.firebaseapp.com',
+      databaseURL: 'https://authentication-70a18.firebaseio.com',
+      storageBucket: 'authentication-70a18.appspot.com',
+      messagingSenderId: '682333809338'
+    });
+  }
+
   render() {
     return (
       <View>
