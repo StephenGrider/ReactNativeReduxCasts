ec057bfdf92bd6e70312f0e9423cc65a21c9906a
stephen grider <ste.grider@gmail.com>
2016-09-13 14:46:32 -0700

    button gets its text from props

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -37,7 +37,9 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)} />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
   return (
     <TouchableOpacity onPress={onPress} style={buttonStyle}>
       <Text style={textStyle}>
-        Click me!!!
+        {children}
       </Text>
     </TouchableOpacity>
   );
