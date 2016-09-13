88e69f12fb4d571115f3e5e41c5ad0fa0d4bedcc
stephen grider <ste.grider@gmail.com>
2016-09-12 20:27:05 -0700

    added scrollview

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -8,7 +8,7 @@ import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -21,9 +21,9 @@ class AlbumList extends Component {
     console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
