991fad1981efcaa174e822ed2b46a4e51d7b1945
stephen grider <ste.grider@gmail.com>
2016-09-13 14:27:00 -0700

    styled da button

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -2,11 +2,36 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
   return (
-    <TouchableOpacity>
-      <Text>Click me!!!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Click me!!!
+      </Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
