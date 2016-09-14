6a7832cdf31231509354da27a76b451d852a3b03
stephen grider <ste.grider@gmail.com>
2016-09-13 22:18:25 -0700

    pass through props

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = { text: '' };
@@ -9,10 +8,9 @@ class LoginForm extends Component {
     return (
       <Card>
         <CardSection>
-          <TextInput
+          <Input
             value={this.state.text}
             onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }}
           />
         </CardSection>
 
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChangeText }) => {
   return (
     <View>
       <Text>{label}</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={{ height: 20, width: 100 }}
+      />
     </View>
   );
 };
