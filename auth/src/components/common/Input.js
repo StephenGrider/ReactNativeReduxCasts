321a0e64bab54ad0dab8988202f9cba583bc1b60
stephen grider <ste.grider@gmail.com>
2016-09-13 22:12:11 -0700

    added reusable text input

---
+++ auth/src/components/common/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = ({ label }) => {
+  return (
+    <View>
+      <Text>{label}</Text>
+    </View>
+  );
+};
+
+export { Input };
--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './Button';
 export * from './Card';
 export * from './CardSection';
 export * from './Header';
+export * from './Input';
