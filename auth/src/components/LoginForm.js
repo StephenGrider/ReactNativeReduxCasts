d23dc2319f1b30709d19f98badb367a0e70dba84
stephen grider <ste.grider@gmail.com>
2016-09-13 22:01:29 -0700

    added controlled component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+  state = { text: '' };
+
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput style={{ height: 20, width: 100 }} />
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 100 }}
+          />
         </CardSection>
 
         <CardSection />
