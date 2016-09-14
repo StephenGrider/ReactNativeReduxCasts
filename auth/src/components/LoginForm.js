d89bd6d8fece7102d4c4a6b0759690453612a796
stephen grider <ste.grider@gmail.com>
2016-09-13 21:57:36 -0700

    added textinput

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <TextInput style={{ height: 20, width: 100 }} />
+        </CardSection>
+
         <CardSection />
 
         <CardSection>
