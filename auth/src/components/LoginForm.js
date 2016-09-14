1c1a1e25ec7e034a33092d890b98cd6cdcf4cbd9
stephen grider <ste.grider@gmail.com>
2016-09-13 22:27:13 -0700

    added styling

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -9,6 +9,7 @@ class LoginForm extends Component {
       <Card>
         <CardSection>
           <Input
+            label="Email"
             value={this.state.text}
             onChangeText={text => this.setState({ text })}
           />
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -2,16 +2,41 @@ import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
 const Input = ({ label, value, onChangeText }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
+        style={inputStyle}
         value={value}
         onChangeText={onChangeText}
         style={{ height: 20, width: 100 }}
       />
     </View>
   );
+};
+
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1
+  },
+  containerStyle: {
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 };
 
 export { Input };
