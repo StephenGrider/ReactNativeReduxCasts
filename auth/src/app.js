7f331b0fef311a87b3e33bf1b17d78cdf42074d6
stephen grider <ste.grider@gmail.com>
2016-09-15 14:28:33 -0700

    loggedin is reflected on state

--- auth/src/app.js
+++ auth/src/app.js
@@ -5,6 +5,8 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
@@ -12,6 +14,14 @@ class App extends Component {
       databaseURL: 'https://authentication-70a18.firebaseio.com',
       storageBucket: 'authentication-70a18.appspot.com',
       messagingSenderId: '682333809338'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
