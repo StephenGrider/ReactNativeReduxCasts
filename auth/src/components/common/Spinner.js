2d8b6611594c9894d8d610bf16e47cdb44657ff3
stephen grider <ste.grider@gmail.com>
2016-09-15 13:38:43 -0700

    added psinner

--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
+const Spinner = ({ size }) => {
   return (
-    <View />
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
   );
-}
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 export { Spinner };
