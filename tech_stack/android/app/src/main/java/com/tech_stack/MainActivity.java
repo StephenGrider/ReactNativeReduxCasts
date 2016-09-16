5fb18ce1c5e21915bd342f34f29b41b479a0fc8d
stephen grider <ste.grider@gmail.com>
2016-09-15 17:04:38 -0700

    generated project

---
+++ tech_stack/android/app/src/main/java/com/tech_stack/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tech_stack;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tech_stack";
+    }
+}
