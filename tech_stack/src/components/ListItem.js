7c02a8bc0d8549d6e646ec09e8e19cf48fcd94f4
stephen grider <ste.grider@gmail.com>
2016-09-17 12:29:05 -0700

    added action creator

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
-
+    console.log(this.props);
+    
     return (
       <CardSection>
         <Text style={titleStyle}>
@@ -23,4 +26,4 @@ const styles = {
   }
 };
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
