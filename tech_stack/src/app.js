6856b9ede7c5e5be5d6803777c05daa2521d8a1a
stephen grider <ste.grider@gmail.com>
2016-09-17 11:50:30 -0700

    added list styling

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -9,7 +9,7 @@ import LibraryList from './components/LibraryList';
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText="Tech Stack" />
         <LibraryList />
       </View>
--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
+    const { titleStyle } = styles;
 
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    );
   }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
