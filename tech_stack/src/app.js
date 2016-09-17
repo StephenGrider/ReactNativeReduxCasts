33b234cc5be51afd65855ce4966c9e61e62de054
stephen grider <ste.grider@gmail.com>
2016-09-17 09:55:14 -0700

    added comps

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -3,11 +3,14 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
---
+++ tech_stack/src/components/common/Input.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
+  return (
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
+      <TextInput
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        autoCorrect={false}
+        style={inputStyle}
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+};
+
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1
+  },
+  containerStyle: {
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+};
+
+export { Input };
