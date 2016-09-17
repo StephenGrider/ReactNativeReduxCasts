e1ae22f6d3eccf63b5aa1699be3b92af05274919
stephen grider <ste.grider@gmail.com>
2016-09-17 12:50:27 -0700

    added reducer logic

--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,4 +1,8 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      return state;
+  }
 };
