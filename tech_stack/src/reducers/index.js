f6e48d068003db8b3b4df947a0b3e614caeca5bb
stephen grider <ste.grider@gmail.com>
2016-09-17 12:12:43 -0700

    added selection reducer

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
