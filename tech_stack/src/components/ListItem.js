46f64f6c2a43bfae644ca63954ff3e49a4ac012b
stephen grider <ste.grider@gmail.com>
2016-09-17 12:44:00 -0700

    Added touchable

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
@@ -7,14 +7,20 @@ import * as actions from '../actions';
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
-    console.log(this.props);
-    
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,3 +1,4 @@
-export default () => {
+export default (state, action) => {
+  console.log(action);
   return null;
 };
