000e69b9aab3cf3b74e803b99d55555ac3ca698a
stephen grider <ste.grider@gmail.com>
2016-10-10 11:49:31 -0700

    action creator

---
+++ manager/src/actions/index.js
@@ -0,0 +1,6 @@
+export const emailChanged = (text) => {
+  return {
+    type: 'email_changed',
+    payload: text
+  };
+};
--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    
+  }
+
   render() {
     return (
       <Card>
@@ -9,6 +13,7 @@ class LoginForm extends Component {
           <Input
             label="Email"
             placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
           />
         </CardSection>
 
