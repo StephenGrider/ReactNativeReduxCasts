f1cab215969d48d29c821b89d4944b6b98dc5d85
stephen grider <ste.grider@gmail.com>
2016-10-10 15:32:24 -0700

    added redux thunk

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
@@ -15,4 +16,9 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => console.log(user));
 };
