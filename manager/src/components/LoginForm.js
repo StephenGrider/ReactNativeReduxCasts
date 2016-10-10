35fe5b13c158123cba8e93a18de56261763f1a5c
stephen grider <ste.grider@gmail.com>
2016-10-10 14:54:17 -0700

    added spread in reducer

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -16,6 +16,7 @@ class LoginForm extends Component {
             label="Email"
             placeholder="email@gmail.com"
             onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
         </CardSection>
 
@@ -37,4 +38,10 @@ class LoginForm extends Component {
   }
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-
+      return { ...state, email: action.payload };
     default:
       return state;
   }
