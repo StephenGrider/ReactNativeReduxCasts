2075bec66278f293497c48225eb351e4c1659eb8
stephen grider <ste.grider@gmail.com>
2016-10-11 15:08:42 -0700

    navigate to form

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -12,12 +13,14 @@ const RouterComponent = () => {
 
       <Scene key="main">
         <Scene
-          onRight={() => console.log('right!!!')}
+          onRight={() => Actions.employeeCreate()}
           rightTitle="Add"
           key="employeeList"
           component={EmployeeList}
           title="Employees"
+          initial
         />
+        <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
       </Scene>
     </Router>
   );
---
+++ manager/src/components/EmployeeCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee Form</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeCreate;
