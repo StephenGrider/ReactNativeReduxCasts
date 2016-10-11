44df80e97cd8eaf86c1340db556e58d29c96c6ca
stephen grider <ste.grider@gmail.com>
2016-10-11 11:38:50 -0700

    added employee list

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key="login" component={LoginForm} title="Please Login" />
+      <Scene key="employeeList" component={EmployeeList} title="Employees" />
     </Router>
   );
 };
---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+        <Text>Employee List</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
