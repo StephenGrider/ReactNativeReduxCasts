689ba602af6518e8bc02b176782f730a96d63086
stephen grider <ste.grider@gmail.com>
2016-10-11 11:44:00 -0700

    on auth navigate to employee list

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -46,4 +47,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.employeeList();
 };
