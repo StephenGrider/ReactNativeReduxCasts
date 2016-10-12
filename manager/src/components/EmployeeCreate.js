3afd4f2259d9cf5aa2c1f985955c786e24278395
stephen grider <ste.grider@gmail.com>
2016-10-12 09:51:52 -0700

    added default value for shift

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -8,7 +8,7 @@ class EmployeeCreate extends Component {
   onButtonPress() {
     const { name, phone, shift } = this.props;
 
-    this.props.employeeCreate({ name, phone, shift });
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
   }
 
   render() {
