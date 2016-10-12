b6f6bfcc304c2864676a3241abe0235812f71233
stephen grider <ste.grider@gmail.com>
2016-10-12 14:46:19 -0700

    navigate to form with props

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -12,6 +12,8 @@ class EmployeeCreate extends Component {
   }
 
   render() {
+    console.log(this.props.employee);
+
     return (
       <Card>
         <CardSection>
--- manager/src/components/ListItem.js
+++ manager/src/components/ListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
+  onRowPress() {
+    Actions.employeeCreate({ employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>
-          {name}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
