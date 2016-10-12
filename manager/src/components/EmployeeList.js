a26c49c3597488964eef1fe11a1060482a042efd
stephen grider <ste.grider@gmail.com>
2016-10-12 11:17:21 -0700

    create datasource

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
