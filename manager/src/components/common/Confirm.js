c3dd61be1a47b9aa0be926b3a31f49252adc287d
stephen grider <ste.grider@gmail.com>
2016-10-12 17:38:42 -0700

    progress on modal component

--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -3,8 +3,26 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
 
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
 };
 
 export { Confirm };
