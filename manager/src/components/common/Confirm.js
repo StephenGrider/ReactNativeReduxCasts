d42c385d85d1f8dd90704ab45f9250e145f42b11
stephen grider <ste.grider@gmail.com>
2016-10-12 17:29:21 -0700

    scaffolded the modal

---
+++ manager/src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = () => {
+
+};
+
+export { Confirm };
--- manager/src/components/common/index.js
+++ manager/src/components/common/index.js
@@ -4,3 +4,4 @@ export * from './CardSection';
 export * from './Header';
 export * from './Input';
 export * from './Spinner';
+export * from './Confirm';
