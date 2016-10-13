b05f07e6643dd0f6e7ad3fe1eef9ae45b394393d
stephen grider <ste.grider@gmail.com>
2016-10-12 17:57:06 -0700

    modal styling

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -41,6 +43,18 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -4,6 +4,8 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+
   return (
     <Modal
       visible={visible}
@@ -11,9 +13,11 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
       animationType="slide"
       onRequestClose={() => {}}
     >
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>
+            {children}
+          </Text>
         </CardSection>
 
         <CardSection>
@@ -23,6 +27,24 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
       </View>
     </Modal>
   );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 };
 
 export { Confirm };
