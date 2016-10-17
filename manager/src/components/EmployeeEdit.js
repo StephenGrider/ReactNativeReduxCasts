56f19f010844e7573d85e255296e911a9b08f096
stephen grider <ste.grider@gmail.com>
2016-10-17 16:21:46 -0700

    can delete employees

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -28,7 +28,9 @@ class EmployeeEdit extends Component {
   }
 
   onAccept() {
+    const { uid } = this.props.employee;
 
+    this.props.employeeDelete({ uid });
   }
 
   onDecline() {
@@ -77,5 +79,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeSave
+  employeeUpdate, employeeSave, employeeDelete
 })(EmployeeEdit);
