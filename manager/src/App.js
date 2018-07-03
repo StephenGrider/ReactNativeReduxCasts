d78d2ac5fb50fddedeb2df2d5c6fc36df0b7a555
stephengrider <ste.grider@gmail.com>
2018-07-03 13:58:27 -0600

    setup

---
+++ manager/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore()}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
