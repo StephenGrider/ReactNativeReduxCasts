657ec05ce82ccb628deb436a4d78308c2d262ecc
stephengrider <ste.grider@gmail.com>
2018-07-03 14:26:14 -0600

    added firebase

--- manager/index.js
+++ manager/index.js
@@ -1,4 +1,15 @@
+import firebase from 'firebase';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+
+const config = {
+  apiKey: 'AIzaSyDsMT4xSksnXXDEVDgZ-ERDIVogCf9y5lA',
+  authDomain: 'manager-1995a.firebaseapp.com',
+  databaseURL: 'https://manager-1995a.firebaseio.com',
+  projectId: 'manager-1995a',
+  storageBucket: 'manager-1995a.appspot.com',
+  messagingSenderId: '455448062565'
+};
+firebase.initializeApp(config);
 
 AppRegistry.registerComponent('manager', () => App);
--- manager/src/App.js
+++ manager/src/App.js
@@ -3,17 +3,6 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-import firebase from 'firebase';
-
-const config = {
-  apiKey: 'AIzaSyCQg64_gQZSuoP7BqTyWF-zKC0MjZXXe4Y',
-  authDomain: 'manager-3c394.firebaseapp.com',
-  databaseURL: 'https://manager-3c394.firebaseio.com',
-  projectId: 'manager-3c394',
-  storageBucket: 'manager-3c394.appspot.com',
-  messagingSenderId: '257060765176'
-};
-firebase.initializeApp(config);
 
 class App extends Component {
   render() {
