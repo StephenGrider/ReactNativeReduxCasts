b93ff8aaffd7d9938b493b353f47aa2b83d4372e
stephengrider <ste.grider@gmail.com>
2018-07-05 15:06:12 -0600

    hide back button

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class EmployeeList extends Component {
+  static navigationOptions = {
+    title: 'Employee List',
+    headerLeft: null
+  };
+
   render() {
     return (
       <View>
