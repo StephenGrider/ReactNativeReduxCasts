d509c7c24fca57a60a86edf9d29d07c14eb3fcd3
stephengrider <ste.grider@gmail.com>
2018-07-09 11:44:12 -0600

    text size

--- manager/src/components/ListItem.js
+++ manager/src/components/ListItem.js
@@ -9,11 +9,18 @@ class ListItem extends Component {
     return (
       <View>
         <CardSection>
-          <Text>{name}</Text>
+          <Text style={styles.titleStyle}>{name}</Text>
         </CardSection>
       </View>
     );
   }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
