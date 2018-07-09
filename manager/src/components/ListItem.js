867b6a746bc137b19d478bc49037b7f291e5374b
stephengrider <ste.grider@gmail.com>
2018-07-09 11:50:46 -0600

    edit

--- manager/src/components/ListItem.js
+++ manager/src/components/ListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import { CardSection } from './common';
+import { navigate } from '../Router';
 
 class ListItem extends Component {
+  onRowPress() {
+    navigate('EmployeeCreate', { employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <View>
-        <CardSection>
-          <Text style={styles.titleStyle}>{name}</Text>
-        </CardSection>
-      </View>
+      <TouchableWithoutFeedback
+        onPress={this.onRowPress.bind(this)}
+      >
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>{name}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
